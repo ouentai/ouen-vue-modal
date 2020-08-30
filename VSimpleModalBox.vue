eb4385eb204664f1b77fd112ce4736b29adaeba8
rky <rky@frontier>
2020-08-30 14:33:55 +0900

    create component

---
+++ VSimpleModalBox.vue
@@ -0,0 +1,151 @@
+<!--
+  コンポーネントの機能
+    - VModal + VModalBox 役割
+    - VModalにネストする必要がない
+    - シンプルにこのコンポーネントだけを設置する目的で使用する
+
+  interface
+    - VModal と VModalBox を参照 : 両方のものを使用できる
+-->
+
+<template>
+  <div :style='{zIndex}' class='modal-frame'>
+    <div
+      v-if='ifState'
+      v-show='open'
+      ref='modal-bg'
+      :style='{backgroundColor:bgColor}'
+      class='modal-bg'
+      @click='clickBg'
+      >
+
+      <div :style='PositionableMixin_style'>
+
+        <div :style='contentStyle' class='vm-box'>
+
+          <slot name='header' v-if='!hideHeader'>
+            <div class='d-flex'>
+              <div class='header-content'></div>
+              <div class='header-close' @click='close'><span>×</span></div>
+            </div>
+            <hr>
+          </slot>
+
+          <slot/>
+
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Positionable from './mixin/Positionable';
+
+export default {
+  name: 'VModalBox',
+  inject: {
+    closeModal : 'closeModal' ,
+  },
+  mixins: [
+    Positionable ,
+  ],
+  props: {
+    // modal part
+    open : Boolean ,
+    zIndex : Number ,
+    openIf : Boolean ,
+    disabledClose : Boolean ,
+    bgColor : {
+      type : String ,
+      default : 'rgba(0,0,0,.5)'
+    },
+
+    // box part
+    hideHeader : Boolean ,
+    size : {
+      type : String ,
+      validator : (val) => ['sm', 'md', 'lg', 'xl', 'auto'].includes(val) ,
+    },
+    width : String ,
+    height : String ,
+    bgColor : String ,
+    borderRadius : String ,
+    padding : String ,
+    overflowX : String ,
+    overflowY : String ,
+  },
+  computed: {
+    // modal part
+    ifState () { return this.openIf ? this.open : true ; },
+
+    // box part
+    contentStyle() {
+      const obj = {
+        borderRadius : this.borderRadius || '.3rem' ,
+        width : this.localWidth ,
+        height : this.height || 'auto' ,
+        padding : this.padding || '1rem' ,
+        overflowX : this.overflowX ,
+        overflowY : this.overflowY ,
+      };
+      if (this.bgColor !== 'null') {
+        obj.backgroundColor = this.bgColor || '#FFF' ;
+      }
+      return obj ;
+    },
+    localWidth() {
+      if (this.width) { return this.width ; }
+      return this.size === 'sm' &&  '300px' ||
+             this.size === 'md' &&  '500px' ||
+             this.size === 'lg' &&  '800px' ||
+             this.size === 'xl' && '1140px' ||
+             this.size === 'auto' && 'auto' ||
+             '500px' ;
+    },
+  },
+  methods: {
+    close() {
+      this.$emit('update:open', false);
+      this.$emit('close');
+    },
+    clickBg(e) {
+      if (!this.disabledClose && e.target===this.$refs['modal-bg']) {
+        this.close();
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.modal-frame {
+  position:fixed;
+  top: 0;
+  left: 0;
+}
+
+.modal-bg {
+  width: 100vw;
+  height: 100vh;
+}
+
+.vm-box {
+  --close-item-width : 40px;
+}
+
+.d-flex {
+  display: flex;
+}
+
+.header-content {
+  width : calc( 100% - var(--close-item-width));
+}
+
+.header-close {
+  width : var(--close-item-width);
+  font-size : 2rem;
+}
+</style>
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export {default as VModal} from './VModal';
 export {default as VModalBox} from './VModalBox';
 export {default as VModalPosition} from './VModalPosition';
+export {default as VSimpleModalBox} from './VSimpleModalBox';
